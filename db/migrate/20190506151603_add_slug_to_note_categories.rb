d1844b612e7cfc0100c614b3ae22de9fb47a4716
gnguyen <gnguyen5464@gmail.com>
2019-05-08 10:32:57 -0400

    Remove migration file add_slug_to_note_categories.rb

--- db/migrate/20190506151603_add_slug_to_note_categories.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToNoteCategories < ActiveRecord::Migration[5.0]
-  def change
-    add_column :note_categories, :slug, :string
-    add_index :note_categories, :slug, unique: true
-  end
-end
